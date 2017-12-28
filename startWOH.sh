e15eccc4e812f623c0f0d7bfaebe2e999b096b37
shevelevw <shevelevw@gmail.com>
2017-12-28 14:52:48 +1300

    Update startWOH.sh

--- startWOH.sh
+++ startWOH.sh
@@ -2,7 +2,7 @@
 exec &> >(tee -a "/var/log/container.log")
 export ENVWAN=`curl -s ifconfig.me`
 export ENVHOSTNAME=$HOSTNAME
-export GOOGLE_APPLICATION_CREDENTIALS=/etc/visumenu/gcs-key.json 
+export GOOGLE_APPLICATION_CREDENTIALS=/etc/proj/gcs-key.json 
 
 #Generate substitutes to generate configs
 printenv|grep '^ENV'|awk 'BEGIN{FS="="}{print("s/${" $1 "}/" $2 "/")}' > /home/replace.sed
@@ -15,14 +15,14 @@ done
 
 # dig +short $ENVDNSNAME |\
 # while read ip; do 
-#   /usr/bin/gcloud dns --project=visumenu-1312 record-sets transaction start --zone=aybell
-#   /usr/bin/gcloud dns record-sets transaction remove --zone=aybell --name "$ENVDNSNAME." --ttl 300 --type A "$ip"
-#   /usr/bin/gcloud dns --project=visumenu-1312 record-sets transaction execute --zone=aybell
+#   /usr/bin/gcloud dns --project=proj-1312 record-sets transaction start --zone=proj
+#   /usr/bin/gcloud dns record-sets transaction remove --zone=proj --name "$ENVDNSNAME." --ttl 300 --type A "$ip"
+#   /usr/bin/gcloud dns --project=proj-1312 record-sets transaction execute --zone=proj
 # done
 
-# /usr/bin/gcloud dns --project=visumenu-1312 record-sets transaction start --zone=aybell
-# /usr/bin/gcloud dns --project=visumenu-1312 record-sets transaction add $ENVWAN --name=$ENVDNSNAME. --ttl=300 --type=A --zone=aybell
-# /usr/bin/gcloud dns --project=visumenu-1312 record-sets transaction execute --zone=aybell
+# /usr/bin/gcloud dns --project=proj-1312 record-sets transaction start --zone=proj
+# /usr/bin/gcloud dns --project=proj-1312 record-sets transaction add $ENVWAN --name=$ENVDNSNAME. --ttl=300 --type=A --zone=proj
+# /usr/bin/gcloud dns --project=proj-1312 record-sets transaction execute --zone=proj
 
 # Wait while DNS will be populated with correct IP
 # while [[ ! "`ping -c 1 $ENVDNSNAME`" || "`dig +short $ENVDNSNAME`" != "$ENVWAN" ]]; do 
@@ -30,17 +30,17 @@ done
 #   echo "Whait while DNS will be populated with correct data"
 # done
 
-# certbot certonly --non-interactive --agree-tos --standalone -d $ENVDNSNAME -m admin@aybell.com
+# certbot certonly --non-interactive --agree-tos --standalone -d $ENVDNSNAME -m admin@proj.com
 a2ensite crawlerapi.conf
 a2dissite 000-default.conf
 
 #Create uploads dir based on conf.json
-jq '.crawler.uploads' /etc/visumenu/conf.json | xargs mkdir -p 
-jq '.crawler.uploads' /etc/visumenu/conf.json | xargs chmod 777 -R 
+jq '.crawler.uploads' /etc/proj/conf.json | xargs mkdir -p 
+jq '.crawler.uploads' /etc/proj/conf.json | xargs chmod 777 -R 
 
 # Creat need folders vm.config['crawler']['uploads'] ....
 # Add DNS names to hosts file
-/bin/echo "127.0.0.1 crawler.aybell.com crawler" >> /etc/hosts
+/bin/echo "127.0.0.1 crawler.proj.com crawler" >> /etc/hosts
 
 #service supervisor stop
 /usr/bin/supervisord
